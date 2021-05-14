292180bbf1effe20f5fd312e9097a660338da176
danggai <donggi9313@naver.com>
2021-05-14 13:41:00 +0900

    - Modified: Refactored functions in refreshWorker to suspend fun

--- app/src/main/java/danggai/app/parcelwhere/worker/RefreshWorker.kt
+++ app/src/main/java/danggai/app/parcelwhere/worker/RefreshWorker.kt
@@ -1,7 +1,7 @@
 package danggai.app.parcelwhere.worker
 
 import android.content.Context
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import danggai.app.parcelwhere.Constant
 import danggai.app.parcelwhere.data.api.ApiRepository
@@ -9,107 +9,59 @@ import danggai.app.parcelwhere.data.db.track.TrackDao
 import danggai.app.parcelwhere.data.db.track.TrackEntity
 import danggai.app.parcelwhere.data.rxbus.RxBusMainSelectAll
 import danggai.app.parcelwhere.util.log
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
-import io.reactivex.subjects.PublishSubject
+import kotlinx.coroutines.coroutineScope
 
 class RefreshWorker (context: Context, workerParams: WorkerParameters, private val api: ApiRepository, private val dao: TrackDao) :
-    Worker(context, workerParams) {
+    CoroutineWorker(context, workerParams) {
 
-    private val rxApiCarrierTracks: PublishSubject<Pair<String, String>> = PublishSubject.create()
-    private val rxDaoUpdate: PublishSubject<TrackEntity> = PublishSubject.create()
+    private suspend fun apiCarrierTracks(carrierId: String, trackId: String) {
+        log.e()
+        val res = api.suspendCarriersTracks(carrierId, trackId)
 
-    private val compositeDisposable = CompositeDisposable()
-
-    init {
-//        compositeDisposable.addAll (
-//            rxApiCarrierTracks
-//                .observeOn(Schedulers.newThread())
-//                .switchMap {
-//                    log.e()
-//                    api.carriersTracks(it.first, it.second)
-//                }
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe({ res ->
-//                    log.e(res)
-//                    when (res.meta.code) {
-//                        Constant.META_CODE_SUCCESS -> {
-//                            rxDaoUpdate.onNext(
-//                                TrackEntity(res.data.trackId, "", res.data.from.name, res.data.carrier.id, res.data.carrier.name, res.data.progresses[res.data.progresses.size-1].time, res.data.state.text, false)
-//                            )
-//                        }
-//                        Constant.META_CODE_BAD_REQUEST,
-//                        Constant.META_CODE_SERVER_ERROR,
-////                    -> {
-////                        log.e()
-////                        lvMakeToast.value = Event(getString(R.string.msg_network_error))
-////                        goBack()
-////                    }
-//                        Constant.META_CODE_NOT_FOUND -> {
-//                            log.e()
-//                        }
-//                        else -> {
-//                            log.e()
-//                        }
-//                    }
-//                }, {
-//                    it.message?.let { msg -> log.e(msg) }
-//                })
-//            ,
-//            rxDaoUpdate
-//                .observeOn(Schedulers.newThread())
-//                .map { item ->
-//                    log.e(item)
-//                    TrackEntity(item.trackId, dao.selectItemNameById(item.trackId), item.fromName, item.carrierId, item.carrierName, item.recentTime, item.recentStatus, item.isRefreshed)
-//                }
-//                .map { item ->
-//                    dao.update(item)
-//                    item
-//                }
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe ({ item ->
-//                    log.e(item)
-//                    RxBusMainSelectAll.getSubject()?.onNext(true)
-//                }, {
-//                    it.message?.let { msg -> log.e(msg) }
-//                })
-//
-//        )
+        log.e(res)
+        when (res.meta.code) {
+            Constant.META_CODE_SUCCESS -> {
+                daoUpdate(TrackEntity(res.data.trackId, "", res.data.from.name, res.data.carrier.id, res.data.carrier.name, res.data.progresses[res.data.progresses.size-1].time, res.data.state.text, false))
+            }
+            Constant.META_CODE_BAD_REQUEST,
+            Constant.META_CODE_SERVER_ERROR,
+            Constant.META_CODE_NOT_FOUND -> {
+                log.e()
+            }
+            else -> {
+                log.e()
+            }
+        }
     }
 
-    override fun doWork(): Result {
+    private suspend fun daoUpdate(item: TrackEntity) {
+        val updatedItem = TrackEntity(item.trackId, dao.suspendSelectItemNameById(item.trackId), item.fromName, item.carrierId, item.carrierName, item.recentTime, item.recentStatus, item.isRefreshed)
+        dao.suspendUpdate(updatedItem)
+
+        log.e(updatedItem)
+        RxBusMainSelectAll.getSubject()?.onNext(true)
+    }
+
+    override suspend fun doWork(): Result {
         log.e()
         try {
-            Observable.just(true)
-                .observeOn(Schedulers.newThread())
-                .switchMap{
-                    log.e()
-                    dao.selectAll()
-                }
-                .map { items ->
-                    for (item in items) {
-                        if (!item.recentStatus!!.contains(Constant.STATE_DELIVERY_COMPLETE)) {
-                            rxApiCarrierTracks.onNext(Pair(item.carrierId, item.trackId))
-                        }
+            coroutineScope {
+                val trackList: List<TrackEntity> = dao.suspendSelectAll()
+                log.e(trackList)
+
+                for (item in trackList) {
+//                    if (true) {
+                    if (!item.recentStatus!!.contains(Constant.STATE_DELIVERY_COMPLETE)) {
+                        log.e()
+                        apiCarrierTracks(item.carrierId, item.trackId)
                     }
-
-                    true
                 }
-                .subscribe({
-                    TODO("개발 중")
-                    log.e()
-                }, {
-
-                })
-            Result.success()
-        } catch (e: Exception) {
+            }
+            return Result.success()
+        } catch (e: java.lang.Exception) {
             log.e(e.message.toString())
-            Result.failure()
+            return Result.failure()
         }
-
-        return Result.success()
     }
 
 }
