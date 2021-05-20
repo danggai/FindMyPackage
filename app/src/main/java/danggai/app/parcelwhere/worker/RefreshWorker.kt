2b6e4e24cbbf5d2ed32547566a950af649231672
danggai <donggi9313@naver.com>
2021-05-20 09:05:25 +0900

    - Modified: RefreshWorker

--- app/src/main/java/danggai/app/parcelwhere/worker/RefreshWorker.kt
+++ app/src/main/java/danggai/app/parcelwhere/worker/RefreshWorker.kt
@@ -14,14 +14,19 @@ import kotlinx.coroutines.coroutineScope
 class RefreshWorker (context: Context, workerParams: WorkerParameters, private val api: ApiRepository, private val dao: TrackDao) :
     CoroutineWorker(context, workerParams) {
 
-    private suspend fun apiCarrierTracks(carrierId: String, trackId: String) {
+    private suspend fun apiCarrierTracks(item: TrackEntity) {
         log.e()
-        val res = api.suspendCarriersTracks(carrierId, trackId)
+        val res = api.suspendCarriersTracks(item.carrierId, item.trackId)
 
         log.e(res)
         when (res.meta.code) {
             Constant.META_CODE_SUCCESS -> {
-                daoUpdate(TrackEntity(res.data.trackId, "", res.data.from.name, res.data.carrier.id, res.data.carrier.name, res.data.progresses[res.data.progresses.size-1].time, res.data.state.text, false))
+                if (item.recentStatus != res.data.state.text) {
+                    log.e()
+                    // 알람 전송
+                }
+                val isRefreshed = item.recentTime != res.data.progresses[res.data.progresses.lastIndex].time
+                daoUpdate(TrackEntity(res.data.trackId, "", res.data.from.name, res.data.carrier.id, res.data.carrier.name, res.data.progresses[res.data.progresses.size-1].time, res.data.state.text, isRefreshed))
             }
             Constant.META_CODE_BAD_REQUEST,
             Constant.META_CODE_SERVER_ERROR,
@@ -53,7 +58,7 @@ class RefreshWorker (context: Context, workerParams: WorkerParameters, private v
 //                    if (true) {
                     if (!item.recentStatus!!.contains(Constant.STATE_DELIVERY_COMPLETE)) {
                         log.e()
-                        apiCarrierTracks(item.carrierId, item.trackId)
+                        apiCarrierTracks(item)
                     }
                 }
             }
