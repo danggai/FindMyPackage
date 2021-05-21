fb3f03684e247202dfd1f174758ed081aa6f0021
danggai <donggi9313@naver.com>
2021-05-21 11:02:07 +0900

    - Implemented: sending notifications when TrackItem updates in RefreshWorker

--- app/src/main/java/danggai/app/parcelwhere/worker/RefreshWorker.kt
+++ app/src/main/java/danggai/app/parcelwhere/worker/RefreshWorker.kt
@@ -1,13 +1,21 @@
 package danggai.app.parcelwhere.worker
 
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import danggai.app.parcelwhere.Constant
+import danggai.app.parcelwhere.R
 import danggai.app.parcelwhere.data.api.ApiRepository
 import danggai.app.parcelwhere.data.db.track.TrackDao
 import danggai.app.parcelwhere.data.db.track.TrackEntity
 import danggai.app.parcelwhere.data.rxbus.RxBusMainSelectAll
+import danggai.app.parcelwhere.ui.track.detail.TrackDetailActivity
+import danggai.app.parcelwhere.util.CarrierUtil
+import danggai.app.parcelwhere.util.CommonFunction
+import danggai.app.parcelwhere.util.PreferenceManager
 import danggai.app.parcelwhere.util.log
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
@@ -25,6 +33,7 @@ class RefreshWorker (context: Context, workerParams: WorkerParameters, private v
                 if (item.recentStatus != res.data.state.text) {
                     log.e()
                     // 알람 전송
+                    sendNoti(item)
                 }
                 val isRefreshed = item.recentTime != res.data.progresses[res.data.progresses.lastIndex].time
                 daoUpdate(TrackEntity(res.data.trackId, "", res.data.from.name, res.data.carrier.id, res.data.carrier.name, res.data.progresses[res.data.progresses.size-1].time, res.data.state.text, isRefreshed))
@@ -46,6 +55,24 @@ class RefreshWorker (context: Context, workerParams: WorkerParameters, private v
 
         log.e(updatedItem)
         RxBusMainSelectAll.getSubject()?.onNext(true)
+    }
+
+    private fun sendNoti(item: TrackEntity) {
+        log.e()
+
+        if (!PreferenceManager.getBooleanAllowGetNoti(applicationContext)) return
+
+        val title = applicationContext.getString(R.string.push_title_exist)
+        val msg = "${item.itemName} (${CarrierUtil.getCarrierName(item.carrierId)} ${item.trackId}) 상품이 '${item.recentStatus}' 상태가 되었습니다."
+
+        val resultIntent = Intent(applicationContext, TrackDetailActivity::class.java)
+            .putExtra(TrackDetailActivity.ARG_TRACK_ENTITY, item)
+        val resultPendingIntent: PendingIntent? = TaskStackBuilder.create(applicationContext).run {
+            addNextIntentWithParentStack(resultIntent)
+            getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        CommonFunction.sendNotification(item.trackId.substring(0,9).toInt(), applicationContext, resultPendingIntent, title, msg)
     }
 
     override suspend fun doWork(): Result {
