11fedacfe0376eb23b2c7df5611f8a513380864c
danggai <donggi9313@naver.com>
2021-05-27 10:54:56 +0900

    - Modified: Implementing Worker Initialization and Cancellation based on activation of ParcelRefresh in Settings

--- app/src/main/java/danggai/app/parcelwhere/ui/setting/SettingFragment.kt
+++ app/src/main/java/danggai/app/parcelwhere/ui/setting/SettingFragment.kt
@@ -5,6 +5,10 @@ import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import androidx.annotation.LayoutRes
+import androidx.work.Data
+import androidx.work.ExistingPeriodicWorkPolicy
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import danggai.app.parcelwhere.BindingFragment
 import danggai.app.parcelwhere.Constant
 import danggai.app.parcelwhere.R
@@ -13,7 +17,9 @@ import danggai.app.parcelwhere.ui.dialog.RxImageDialog
 import danggai.app.parcelwhere.util.EventObserver
 import danggai.app.parcelwhere.util.PreferenceManager
 import danggai.app.parcelwhere.util.log
+import danggai.app.parcelwhere.worker.RefreshWorker
 import org.koin.androidx.viewmodel.ext.android.getViewModel
+import java.util.concurrent.TimeUnit
 
 class SettingFragment : BindingFragment<SettingFragmentBinding>() {
 
@@ -111,6 +117,19 @@ class SettingFragment : BindingFragment<SettingFragmentBinding>() {
             activity?.let { act ->
                 PreferenceManager.setBoolean(act, Constant.PREF_AUTO_REFRESH, !allowed)
                 mVM.lvIsAllowAutoRefresh.value = !allowed
+                val workManager = WorkManager.getInstance(act)
+                if (!allowed) {
+                    val refreshPeriod = PreferenceManager.getLongAutoRefreshPeriod(act)
+                    val workRequest = PeriodicWorkRequestBuilder<RefreshWorker>(refreshPeriod, TimeUnit.MINUTES)
+                        .setInputData(Data.Builder()
+                            .putLong(Constant.WORKER_DATA_REFRESH_PERIOD, refreshPeriod)
+                            .build()
+                        )
+                        .build()
+                    workManager.enqueueUniquePeriodicWork(Constant.WORKER_UNIQUE_NAME_AUTO_REFRESH, ExistingPeriodicWorkPolicy.REPLACE, workRequest)
+                } else {
+                    workManager.cancelAllWork()
+                }
             }
         })
 
