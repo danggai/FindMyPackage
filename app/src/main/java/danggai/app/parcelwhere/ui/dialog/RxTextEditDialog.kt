0be2a1d64f122c598f60d2a5b0e79904130bc34a
danggai <donggi9313@naver.com>
2021-06-07 16:56:23 +0900

    - Implemented: TextEditDialog

--- app/src/main/java/danggai/app/parcelwhere/ui/dialog/RxTextEditDialog.kt
+++ app/src/main/java/danggai/app/parcelwhere/ui/dialog/RxTextEditDialog.kt
@@ -8,29 +8,29 @@ import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.FragmentActivity
 import danggai.app.parcelwhere.R
-import danggai.app.parcelwhere.databinding.DialogDefaultBinding
+import danggai.app.parcelwhere.databinding.DialogTextEditBinding
 import danggai.app.parcelwhere.util.log
 import io.reactivex.Observable
 import io.reactivex.subjects.PublishSubject
-import kotlinx.android.synthetic.main.dialog_default.*
+import kotlinx.android.synthetic.main.dialog_text_edit.*
 
-class RxImageDialog(builder: Builder) {
+class RxTextEditDialog(builder: Builder) {
 
     private val mActivity   = builder.activity
-    private val mImage      = builder.img
     private var mMsg        = builder.msg
+    private var mHint       = builder.hint
     private val mConfirm    = builder.confirm
     private val mCancel     = builder.cancel
     private val mConfirmOnly= builder.confirmOnly
 
     private val mDialog = getDialog()
 
-    private fun getDialog(): ImageDialog {
-        return ImageDialog(mActivity, mImage, mMsg, mConfirm, mCancel, mConfirmOnly)
+    private fun getDialog(): TextEditDialog {
+        return TextEditDialog(mActivity, mMsg, mHint, mConfirm, mCancel, mConfirmOnly)
     }
 
-    fun show(): Observable<Boolean> {
-        val result: PublishSubject<Boolean> = PublishSubject.create()
+    fun show(): Observable<String> {
+        val result: PublishSubject<String> = PublishSubject.create()
         mDialog.setCancelable(false)
         mDialog.requestRx(result)
         try {
@@ -42,44 +42,41 @@ class RxImageDialog(builder: Builder) {
         return result
     }
 
-    class Builder(
-        internal val activity: FragmentActivity,
-        internal val img: Int?,
-        internal val msg: String,
-        internal val confirm: String,
-        internal val cancel: String,
-        internal val confirmOnly: Boolean,
-    ) {
-        fun build(): RxImageDialog = RxImageDialog(this)
+    class Builder(internal val activity: FragmentActivity,
+                  internal val hint: String,
+                  internal val msg: String,
+                  internal val confirm: String,
+                  internal val cancel: String,
+                  internal val confirmOnly: Boolean) {
+        fun build(): RxTextEditDialog = RxTextEditDialog(this)
     }
 }
 
-private class ImageDialog(
+private class TextEditDialog(
     mActivity: Activity,
-    private var mImage: Int?,
     private var mMsg: String,
+    private var mHint: String,
     private val mConfirm: String,
     private val mCancel: String,
-    private val mConfirmOnly: Boolean,
-) : AlertDialog(mActivity) {
+    private val mConfirmOnly: Boolean) : AlertDialog(mActivity) {
 
-    private lateinit var binding: DialogDefaultBinding
+    private lateinit var binding: DialogTextEditBinding
 
-    private var mPublishSubject: PublishSubject<Boolean> ?= null
-    private var mResult = false
+    private var mPublishSubject: PublishSubject<String> ?= null
+    private var mResult: String = ""
 
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DialogDefaultBinding.inflate(layoutInflater)
+
+        binding = DialogTextEditBinding.inflate(layoutInflater)
         
-        setContentView(R.layout.dialog_default)
+        setContentView(R.layout.dialog_text_edit)
         window!!.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
         val params = window!!.attributes
         params.gravity = Gravity.CENTER
 
         setMessage(mMsg)
-        setImage(mImage)
         controlBtn()
     }
 
@@ -94,54 +91,48 @@ private class ImageDialog(
 
     fun setMessage(msg: String) {
         mMsg = msg
-        tv_message.let {
-            it.text = mMsg
-        }
+        tv_message.text = mMsg
     }
 
-    fun setImage(img: Int?) {
-        mImage = img
-        iv_image.let {
-            if (img == null) {
-                it.visibility = View.GONE
-            } else {
-                it.visibility = View.VISIBLE
-                it.setImageResource(img)
-            }
-        }
+    fun setHint(hint: String) {
+        mHint = hint
+        text_input.hint = mHint
     }
 
     private fun controlBtn() {
         if (mConfirmOnly) {
-            mr_cancel.visibility = View.GONE
-            mr_confirm.visibility = View.VISIBLE
+            tv_cancel.visibility = View.GONE
+            tv_confirm.visibility = View.VISIBLE
 
             tv_confirm.text = mConfirm
-            mr_confirm.setOnClickListener {
-                mResult = true
+            tv_confirm.setOnClickListener {
+                log.e(text_input.text.toString())
+                mResult = text_input.text.toString()
                 dismiss()
             }
         } else {
-            mr_cancel.visibility = View.VISIBLE
-            mr_confirm.visibility = View.VISIBLE
+            tv_cancel.visibility = View.VISIBLE
+            tv_confirm.visibility = View.VISIBLE
             tv_confirm.text = mConfirm
-            mr_confirm.setOnClickListener {
-                mResult = true
+            tv_confirm.setOnClickListener {
+                log.e()
+                mResult = text_input.text.toString()
                 dismiss()
             }
             tv_cancel.text = mCancel
-            mr_cancel.setOnClickListener {
-                mResult = false
+            tv_cancel.setOnClickListener {
+                log.e()
+                mResult = ""
                 dismiss()
             }
         }
     }
 
-    fun requestRx(result: PublishSubject<Boolean>) {
+    fun requestRx(result: PublishSubject<String>) {
         mPublishSubject = result
     }
 
-    private fun sendResult (result: Boolean) {
+    private fun sendResult (result: String) {
         log.e()
         mPublishSubject?.onNext(result)
 
