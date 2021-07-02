b3adab42d2c46cdad7ab377ac64e9ef118a16fcb
danggai <donggi9313@naver.com>
2021-07-02 16:13:57 +0900

    - Implemented: Test Example with Mockito

--- app/src/test/java/danggai/app/parcelwhere/ExampleUnitTest.kt
+++ app/src/test/java/danggai/app/parcelwhere/ExampleUnitTest.kt
@@ -1,17 +1,32 @@
 package danggai.app.parcelwhere
 
+import danggai.app.parcelwhere.data.local.Carrier
+import danggai.app.parcelwhere.data.local.Tracks
+import danggai.app.parcelwhere.util.CarrierUtil
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+@RunWith(MockitoJUnitRunner::class)
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun carrierUtilTest() {
+        val carrier = Mockito.mock(Carrier::class.java)
+
+        Mockito.`when`(carrier.id).thenReturn("kr.cjlogistics")
+        Mockito.`when`(carrier.idx).thenReturn(0)
+        Mockito.`when`(carrier.name).thenReturn("CJ대한통운")
+        Mockito.`when`(carrier.tel).thenReturn("+8215881255")
+
+        assertTrue( CarrierUtil.getCarrierName(carrier.id) == carrier.name )
+        assertTrue( CarrierUtil.getCarrierId(carrier.name) == carrier.id )
     }
 }
